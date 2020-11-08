fd4c27adb818e29cae49d32eddb40d6498e7bad2
jose barreto teixeira de paula <jose.barreto@fieldcorp.com.br>
2020-11-08 19:58:47 -0300

    using generics types

---
+++ app/js/views/NegociacoesView.js
@@ -0,0 +1,33 @@
+class NegociacoesView extends View {
+    template(model) {
+        return `
+        <table class="table table-hover table-bordered">
+        <thead>
+            <tr>
+                <th>DATA</th>
+                <th>QUANTIDADE</th>
+                <th>VALOR</th>
+                <th>VOLUME</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            ${model.paraArray().map(negocicao => {
+            return `
+                <tr>
+                <td>${negocicao.data.getDay()}/${negocicao.data.getMonth() + 1}/${negocicao.data.getFullYear()}</td>
+                <td>${negocicao.quantidade}</td>
+                <td>${negocicao.valor}</td>
+                <td>${negocicao.volume}</td>
+                </tr>
+                `;
+        }).join('')}
+        </tbody>
+
+        <tfoot>
+        </tfoot>
+    </table>
+
+        `;
+    }
+}
